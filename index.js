188c13958f8acad639fcb3cfc56a9b12ac8ecce2
Lindokuhle Yende <148907013+LindokuhleYende@users.noreply.github.com>
2023-11-19 17:16:41 +0200

    added weather forecast

--- index.js
+++ index.js
@@ -12,19 +12,24 @@ function displayTemperature(response) {
   condition.innerHTML = `,${response.data.condition.description}`
   let icon = document.getElementById("condition-icon")
   icon.setAttribute("src",`${response.data.condition.icon_url}`);
+  let date = new Date(response.data.time * 1000);
+  getForecast(response.data.city);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
-
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
+  displayCity(city);
 }
 
+function displayCity(city){
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+  axios.get(apiUrl).then(displayTemperature);
+
+}
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -51,11 +56,50 @@ function formatDate(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[date.getDay()];
+}
+
+function getForecast(city){
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response){
+  console.log(response.data);
+
+  
+  let forecastHtml = " ";
+  response.data.daily.forEach(function(day,index){
+    if (index>0 && index<6){
+      forecastHtml += 
+      `
+      <div class="weather-forecast-day">
+         <div class="weather-forecast-date"> ${formatDay(day.time)}</div>
+         <div class="weather-forecast-icon">
+           <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+         </div>
+         <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+               <strong>${Math.round(day.temperature.maximum)}°C</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C</div>
+         </div>
+      </div>
+`;
+
+    }
+  });
+  let forecastElement = document.querySelector("#forecast")
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
-
 currentDateELement.innerHTML = formatDate(currentDate);
+displayCity("Soweto");
